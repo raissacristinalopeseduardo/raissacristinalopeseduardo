35d67fa8bbea1d4e4d285f8b1372da49e1973e4e
raissacristinalopeseduardo <raissalopes2209200823@gmail.com>
2025-04-22 08:54:40 -0300

    sistema de nome.py

---
+++ sistema de nome.py
@@ -0,0 +1,31 @@
+from flet import *
+
+def main(page:Page):
+     
+    page.window_center()
+    page.window.width= 1200
+    page.window.height= 650
+    page.bgcolor= "#FFFFFF"
+    page.window.maximizable = False
+    page.window.resizable = False
+
+
+    t= TextField(label= "Nome")
+    b= FilledButton(text="salvar",on_click= lambda e: salvar(e))
+    g= Text("",size=50)
+
+    def salvar(e):
+
+        if t.value == "Raissa":
+
+            g.value= "CORRETO"
+            g.color =colors.GREEN_100
+
+        else:
+            g.value= "ERRADO"
+            g.color=colors.RED_100
+
+
+    page.add(t, b, g )
+
+app(target = main)
