db135fa4905dea0322d898bbc05400afe0cb9fb0
raissacristinalopeseduardo <raissalopes2209200823@gmail.com>
2025-04-23 08:02:37 -0300

    Sistema de nome.py

--- Sistema de nome.py
+++ Sistema de nome.py
@@ -1,17 +1,12 @@
 from flet import *
+from sqlite3 import Error
+from conexao import *
 
 def main(page:Page):
-     
-    page.window_center()
-    page.window.width= 1200
-    page.window.height= 650
-    page.bgcolor= "#FFFFFF"
-    page.window.maximizable = False
-    page.window.resizable = False
-
 
     t= TextField(label= "Nome", autofocus= True)
     b= FilledButton(text="salvar",on_click= lambda e: salvar(e))
+    c = FilledButton(text="Conect",on_click= lambda e: create(e))
     g= Text("",size=50)
 
     def salvar(e):
@@ -19,16 +14,34 @@ def main(page:Page):
         if t.value == "Raissa":
 
             g.value= "CORRETO"
-            g.color =colors.GREEN_100
+            g.color ="Green"
 
         else:
             g.value= "ERRADO"
-            g.color=colors.RED_100
+            g.color="Red"
 
         t.value=""
+      
         page.update()
 
+    def create(e):
+        nome = t.value
 
-    page.add(t, b, g )
+        t.value= ""
+
+        if nome:
+            conn, salvar = conectar_banco()
+            if conn:
+                result = inserir_nome(conn, nome)
+                g.value = result
+                conn.close()
+            else:
+                g.value = salvar
+        else:
+            g.value = "Por favor, insira seu nome!"
+        
+        page.update
+
+    page.add(t, b, c, g)
 
 app(target = main)
